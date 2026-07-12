298627e4d62a1f03dc2eb00bbe9be5840a049d21
kaushalbaitha7 <kaushalbaitha74@gmail.com>
2026-07-12 18:57:47 +0530

    Updated error

---
+++ src/app/language/c/introduction/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Coming Soon
+      </h1>
+    </main>
+  );
+}
